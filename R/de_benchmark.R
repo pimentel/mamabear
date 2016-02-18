b35519183b668a808c2d7d615f5e60870b58d454
Harold Pimentel <haroldpimentel@gmail.com>
2016-02-18 10:29:33 -0800

    re factor and add eFDR versus FDR #vc

--- R/de_benchmark.R
+++ R/de_benchmark.R
@@ -43,7 +43,7 @@ new_de_benchmark <- function(de_list, de_labels, oracle) {
   m_qval <- melt_by(all_res, "qval")
 
   all_res <- all_res %>%
-    inner_join(data.table::data.table(oracle), by = "target_id")
+    dplyr::inner_join(data.table::data.table(oracle), by = "target_id")
 
   ret <- list(
     all_data = as_df(all_res),
@@ -54,6 +54,14 @@ new_de_benchmark <- function(de_list, de_labels, oracle) {
   ret
 }
 
+#' Plot the (estimated) FDR against the TPR
+#'
+#' Plot the (estimated) FDR against the TPR.
+#' Currently can only do the estimated FDR.
+#' Will eventually support the true FDR
+#'
+#' @param de_bench a \code{de_benchmark} object
+#' @return a \code{ggplot} object
 #' @export
 fdr_tpr_plot <- function(de_bench) {
   stopifnot( is(de_bench, "de_benchmark") )
@@ -64,15 +72,13 @@ fdr_tpr_plot <- function(de_bench) {
     mutate(tpr = cummean(is_de)) %>%
     ggplot(aes(estimate, tpr, group = method)) +
       geom_line(aes(colour = method)) +
-      xlab("FDR") +
+      xlab("eFDR") +
       ylab("TPR") +
       xlim(0, 1) +
       ylim(0, 1)
 }
 
-
-#' @export
-fdr_nde_plot <- function(de_bench, estimate = TRUE) {
+calculate_fdr <- function(de_bench) {
   stopifnot( is(de_bench, "de_benchmark") )
 
   n_true_de <- sum(de_bench$all_data$is_de)
@@ -84,27 +90,67 @@ fdr_nde_plot <- function(de_bench, estimate = TRUE) {
   qvals <- dplyr::mutate(qvals, method = sub("qval_", "", method))
   qvals <- dplyr::rename(qvals, qval = estimate)
 
-  pvals <- inner_join(pvals, qvals, by = c("target_id", "method"))
+  pvals <- dplyr::inner_join(pvals, qvals, by = c("target_id", "method"))
 
-  plt <- pvals %>%
-    dplyr::mutate(method = sub("pval_", "", method)) %>%
-    group_by(method) %>%
-    arrange(estimate) %>%
-    dplyr::mutate(nde = 1:n(), tFDR = cummean(!is_de)) %>%
-    ggplot(aes(nde, estimate, group = method))
+  pvals <- dplyr::mutate(pvals, method = sub("pval_", "", method))
+  pvals <- dplyr::group_by(pvals, method)
+  pvals <- dplyr::arrange(pvals, estimate)
+  pvals <- dplyr::mutate(pvals, nde = 1:n(), tFDR = cummean(!is_de))
+
+  pvals
+}
+
+#' @export
+fdr_efdr_plot <- function(de_bench) {
+  stopifnot( is(de_bench, "de_benchmark") )
+
+  pvals <- calculate_fdr(de_bench)
+
+  p <- ggplot(pvals, aes(qvals, tFDR))
+  p <- p + geom_line(aes(color = method, linetype = method))
+  p <- p + xlab("eFDR")
+  p <- p + ylab("FDR")
+
+  p
+}
+
+#' @export
+fdr_nde_plot <- function(de_bench, estimate = TRUE) {
+  stopifnot( is(de_bench, "de_benchmark") )
+
+  # n_true_de <- sum(de_bench$all_data$is_de)
+  # message('Intersection of targets: ', nrow(de_bench$all_data))
+  # message('Number of truly DE: ', n_true_de)
+  #
+  # pvals <- dplyr::mutate(de_bench$m_pval, method = sub("pval_", "", method))
+  # qvals <- dplyr::select(de_bench$m_qval, target_id, method, estimate)
+  # qvals <- dplyr::mutate(qvals, method = sub("qval_", "", method))
+  # qvals <- dplyr::rename(qvals, qval = estimate)
+  #
+  # pvals <- inner_join(pvals, qvals, by = c("target_id", "method"))
+
+  # plt <- pvals %>%
+  #   dplyr::mutate(method = sub("pval_", "", method)) %>%
+  #   group_by(method) %>%
+  #   arrange(estimate) %>%
+  #   dplyr::mutate(nde = 1:n(), tFDR = cummean(!is_de)) %>%
+  #   ggplot(aes(nde, estimate, group = method))
+  pvals <- calculate_fdr(de_bench)
+  plt <- ggplot(pvals, aes(nde, estimate, group = method))
+
   if (estimate) {
     plt <- plt + geom_line(aes(nde, qval, colour = method), linetype = 3)
   }
 
   plt <- plt +
-      geom_line(aes(nde, tFDR, colour = method, linetype = method), size = 0.8,
-        alpha = 0.8) +
-      geom_vline(xintercept = n_true_de, linetype = 3) +
-      geom_hline(yintercept = 0.10, linetype = 3) +
-      #theme_bw() +
-      xlab("Number of features called DE") +
-      ylab("FDR") +
-      ylim(0, 1)
+    geom_line(aes(nde, tFDR, colour = method, linetype = method),
+      size = 0.8, alpha = 0.8) +
+    geom_vline(xintercept = n_true_de, linetype = 3) +
+    geom_hline(yintercept = 0.10, linetype = 3) +
+    #theme_bw() +
+    xlab("Number of features called DE") +
+    ylab("FDR") +
+    ylim(0, 1)
 
   plt
 }
@@ -114,7 +160,7 @@ pval_distribution <- function(de_bench) {
   stopifnot( is(de_bench, "de_benchmark") )
 
   de_bench$m_pval %>%
-    ggplot(aes(estimate, y = ..density..)) +
+    ggplot(aes(estimate, y = ..density..)) + # nolint
     geom_histogram(binwidth = 0.02) +
     facet_wrap( ~ method )
 }
